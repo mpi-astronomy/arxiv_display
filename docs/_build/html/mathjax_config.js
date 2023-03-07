266e7cfc2656f12d89053affba8ad4269849a16f
Morgan Fouesneau <morgan.fouesneau@astro.washington.edu>
2023-03-07 16:46:30 +0100

    new carousel with flickity

--- docs/_build/html/mathjax_config.js
+++ docs/_build/html/mathjax_config.js
@@ -4,18 +4,31 @@
 // Latex extension list http://docs.mathjax.org/en/latest/input/tex/extensions/index.html
 MathJax = {
     loader: {
-    load: ['ui/menu', '[tex]/all-packages', '[tex]/physics',
+        load: ['ui/menu', '[tex]/all-packages', '[tex]/physics',
             '[tex]/ams', '[tex]/gensymb', '[tex]/noundefined',
-            '[tex]/noerrors']
+            '[tex]/noerrors', '[tex]/boldsymbol', '[tex]/textcomp', '[tex]/textmacros'
+        ]
     },
     tex: {
-    packages: {'[+]': ['physics', 'ams', 'gensymb', 'noundefined']},
-    inlineMath: [['$', '$'], ['\\(', '\\)']],
-    processEscapes: false,
-    noundefined: {
-        color: 'red',
-        background: '',
-        size: ''
-    }
+        packages: { '[+]': ['physics', 'ams', 'gensymb', 'noundefined', 'textcomp', 'bbox', 'boldsymbol', 'textmacros'] },
+        inlineMath: [['$', '$'], ['\\(', '\\)']],
+        processEscapes: false,
+        noundefined: {
+            color: 'red',
+            background: '',
+            size: ''
+        },
+        macros: {
+            RR: "{\\bf R}",
+            bold: ["{\\bf #1}", 1],
+            text: ["{#1}", 1],
+            textalpha: "{\\alpha}",
+            textbeta: "{\\beta}",
+            textgamma: "{\\gamma}",
+            textdelta: "{\\delta}",
+            xspace: "{}",
+            mbox: ["{#1}", 1]
+
+        }
     },
 };
