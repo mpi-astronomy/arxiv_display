64dfc091bdfe500428179b402a659c8ed6c4367f
Morgan Fouesneau <morgan.fouesneau@astro.washington.edu>
2023-03-24 17:59:47 +0100

    updated arxiv_on_deck

--- docs/_build/html/mathjax_config.js
+++ docs/_build/html/mathjax_config.js
@@ -12,7 +12,9 @@ MathJax = {
     tex: {
         packages: { '[+]': ['physics', 'ams', 'gensymb', 'noundefined', 'textcomp', 'bbox', 'boldsymbol', 'textmacros'] },
         inlineMath: [['$', '$'], ['\\(', '\\)']],
-        processEscapes: false,
+        displayMath: [["$$", "$$"], ["\\[", "\\]"]],
+        processEscapes: true,
+        processEnvironments: true,
         noundefined: {
             color: 'red',
             background: '',
@@ -27,8 +29,62 @@ MathJax = {
             textgamma: "{\\gamma}",
             textdelta: "{\\delta}",
             xspace: "{}",
-            mbox: ["{#1}", 1]
-
+            mbox: ["{#1}", 1],
+            Alpha: "\\mbox{A}",
+            Beta: "\\mbox{B}",
+            Epsilon: "\\mbox{E}",
+            Zeta: "\\mbox{Z}",
+            Eta: "\\mbox{H}",
+            Iota: "\\mbox{I}",
+            Kappa: "\\mbox{K}",
+            Mu: "\\mbox{M}",
+            Nu: "\\mbox{N}",
+            Omicron: "\\mbox{O}",
+            Rho: "\\mbox{P}",
+            Tau: "\\mbox{T}",
+            Chi: "\\mbox{X}",
+            and: "\\mbox{&}",
+            or: "\\lor",
+            exist: "\\exists",
+            empty: "\\emptyset",
+            P: "\\mbox{P}",
+            tan: "\\operatorname{tg}",   // tangent
+            tg: "\\operatorname{tg}",    // tangent
+            cot: "\\operatorname{ctg}",  // cotangent
+            ctg: "\\operatorname{ctg}",  // cotangent
+            csc: "\\operatorname{cosec}",     // cosecant
+            cosec: "\\operatorname{cosec}",   // cosecant
+            arctan: "\\operatorname{arctg}",  // arctangent
+            arctg: "\\operatorname{arctg}",   // arctangent
+            arccot: "\\operatorname{arcctg}",      // arc cotangent
+            arcctg: "\\operatorname{arcctg}",      // arc cotangent
+            arcsec: "\\operatorname{arcsec}",      // arc secant
+            arccsc: "\\operatorname{arccosec}",    // arc cosecant
+            arccosec: "\\operatorname{arccosec}",  // arc cosecant
+            sh: "\\operatorname{sh}",     // hyperbolic sine
+            ch: "\\operatorname{ch}",     // hyperbolic cosine
+            th: "\\operatorname{th}",     // hyperbolic tangent
+            cth: "\\operatorname{cth}",   // hyperbolic cotangent
+            sinh: "\\operatorname{sh}",   // hyperbolic синус
+            cosh: "\\operatorname{ch}",   // hyperbolic cosine
+            tanh: "\\operatorname{th}",   // hyperbolic tangent
+            coth: "\\operatorname{cth}",  // hyperbolic cotangent
+            sgn: "\\operatorname{sgn}",
+            mod: "\\operatorname{mod}",
+            ge: "\\geqslant",
+            le: "\\leqslant",
+            geq: "\\geqslant",
+            leq: "\\leqslant",
+            N: "\\mathbb{N}",
+            R: "\\mathbb{R}",
+            Q: "\\mathbb{Q}",
+            Z: "\\mathbb{Z}",
+            C: "\\mathbb{C}",
+            H: "\\mathbb{H}",
+            P: "\\mathbb{P}",
+            dmtr: "\\unicode{x2300}", // diameter sign
+            deg: "\\unicode{xb0}",    // degree sign
+            celdeg: "\\unicode{x2103}"   // degree Celsius sign
         }
     },
 };
